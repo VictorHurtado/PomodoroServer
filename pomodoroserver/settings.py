6f896e5ecbe360e19f07674394181533b1367c94
VictorHurtado <victor2012lopezxd@gmail.com>
2022-08-13 11:16:47 -0500

    cambios de puerto

--- pomodoroserver/settings.py
+++ pomodoroserver/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 SWAGGER_SETTINGS = {
